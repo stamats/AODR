b3c9594f0ab54431a5ee3a016fe4fa465fc5974d
stamats <matthias.kohl@stamats.de>
2022-03-15 13:09:14 +0100

    Added English versions of the scripts

---
+++ English/Affymetrix/AffymetrixPackages.R
@@ -0,0 +1,58 @@
+###############################################################################
+## Required packages for the analysis of Affymetrix microarray data
+###############################################################################
+
+if(!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+library(BiocManager)
+
+if(!require("affy", quietly = TRUE))
+  install("affy", update = FALSE)
+
+if(!require("hgu133plus2cdf", quietly = TRUE))
+  install("hgu133plus2cdf", update = FALSE)
+
+if(!require("arrayQualityMetrics", quietly = TRUE))
+  install("arrayQualityMetrics", update = FALSE)
+
+if(!require("vsn", quietly = TRUE))
+  install("vsn", update = FALSE)
+
+if(!require("RobLoxBioC", quietly = TRUE))
+  install.packages("RobLoxBioC")
+
+if(!require("plier", quietly = TRUE))
+  install("plier", update = FALSE)
+
+if(!require("genefilter", quietly = TRUE))
+  install("genefilter", update = FALSE)
+
+if(!require("MKomics", quietly = TRUE))
+  install.packages("MKomics")
+
+if(!require("exactRankTests", quietly = TRUE))
+  install.packages("exactRankTests")
+
+if(!require("multtest", quietly = TRUE))
+  install("multtest", update = FALSE)
+
+if(!require("MKinfer", quietly = TRUE))
+  install.packages("MKinfer")
+
+if(!require("circlize", quietly = TRUE))
+  install.packages("circlize")
+
+if(!require("RColorBrewer", quietly = TRUE))
+  install.packages("RColorBrewer")
+
+if(!require("ComplexHeatmap", quietly = TRUE))
+  install("ComplexHeatmap", update = FALSE)
+
+if(!require("org.Hs.eg.db", quietly = TRUE))
+  install("org.Hs.eg.db", update = FALSE)
+
+if(!require("hgu133plus2.db", quietly = TRUE))
+  install("hgu133plus2.db", update = FALSE)
+
+if(!require("GO.db", quietly = TRUE))
+  install("GO.db", update = FALSE)
